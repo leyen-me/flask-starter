2a53af120c98199d3ef748d2bd9e5b559847ea6b
DIFFFFFFT <672228275@qq.com>
2023-11-02 09:12:33 +0800

    优化REDIS连接

--- db/__init__.py
+++ db/__init__.py
@@ -9,6 +9,7 @@ db = SQLAlchemy()
 
 
 # =========================================缓存数据库==========================================
-redis_pool = r.ConnectionPool(host=CONFIG["REDIS"]["HOST"], port=CONFIG["REDIS"]["PORT"], db=CONFIG["REDIS"]["DB"])
+redis_pool = r.ConnectionPool(host=CONFIG["REDIS"]["HOST"], port=CONFIG["REDIS"]["PORT"], db=CONFIG["REDIS"]["DB"],
+                              password=CONFIG.get('REDIS', {}).get('PASSWORD'))
 redis = r.Redis(connection_pool=redis_pool)
 # =========================================缓存数据库===========================================
